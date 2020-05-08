b4591137f0a9a3b6c7b772781163a2c0c7b7411f
YURIM MAENG <yurimmaeng@YURIMs-MBP.home>
2020-05-07 22:11:07 -0700

    Updates

---
+++ precache-manifest.19d2e172228fea053e442e9e4d18a3c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68abddc803bf2c5040d50d2af312b55f",
+    "url": "/realhw19/index.html"
+  },
+  {
+    "revision": "b379a004faf87cfa3e77",
+    "url": "/realhw19/static/css/main.3a0af899.chunk.css"
+  },
+  {
+    "revision": "6209bb577080221c1acc",
+    "url": "/realhw19/static/js/2.284b675b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/realhw19/static/js/2.284b675b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b379a004faf87cfa3e77",
+    "url": "/realhw19/static/js/main.07298882.chunk.js"
+  },
+  {
+    "revision": "d49dc3a97c0fc08069f7",
+    "url": "/realhw19/static/js/runtime-main.aa00d768.js"
+  }
+]);
